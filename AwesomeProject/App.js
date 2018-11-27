bbdefd22cf4e690d6de21df256f85b1865bcc912
HD <hdontheline@gmail.com>
2018-11-27 22:51:07 +0000

    Align items

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -5,10 +5,11 @@ export default class FlexDirectionBasics extends Component {
   render() {
     return ( 
       <View style={{flex: 1, flexDirection: 'column',
-      justifyContent: 'space-between',}}>
+      justifyContent: 'center',
+      alignItems: 'stretch',}}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+        <View style={{height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
